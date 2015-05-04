43dc155ca098225082976387456b572bfc19bc55
nurifdedagic <nurifdedagic@gmail.com>
2015-05-04 23:28:43 +0200

    commit8

---
+++ UcitavanjeStranica.js
@@ -0,0 +1,14 @@
+function ucitajStranicu(stranica){
+				var x=new XMLHttpRequest();
+				x.onreadystatechange=function(){
+					if(x.readyState==4 && x.status==200){
+						document.open();
+						document.write(x.responseText);
+						document.close();
+					}
+				}	
+				x.open("GET", stranica, true);
+				x.send();
+return false;
+                                				
+			}
